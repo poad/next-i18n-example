6cdc3b0c3843800b8ba67e8c2c5f4d74593d7c78
Kenji Saito <ken-yo@mbr.nifty.com>
2023-05-03 23:28:27 +0900

    Bumps node modules

--- app/next.config.js
+++ app/next.config.js
@@ -13,11 +13,8 @@ const config = {
   eslint: {
     ignoreDuringBuilds: true,
   },
-  transpilePackages: ["@mui/system", "@mui/material", "@mui/icons-material"],
+  transpilePackages: ["@mui/icons-material"],
   modularizeImports: {
-    "@mui/material/?(((\\w*)?/?)*)": {
-      transform: "@mui/material/{{ matches.[1] }}/{{member}}",
-    },
     "@mui/icons-material/?(((\\w*)?/?)*)": {
       transform: "@mui/icons-material/{{ matches.[1] }}/{{member}}",
     },
