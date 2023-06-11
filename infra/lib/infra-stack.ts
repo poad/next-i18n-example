edff6d72206e6d9615a59b1c55f537c3cc3cd4ec
Kenji Saito <ken-yo@mbr.nifty.com>
2023-06-11 21:06:17 +0900

    Fix the S3 access error

--- infra/lib/infra-stack.ts
+++ infra/lib/infra-stack.ts
@@ -1,11 +1,12 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as origin from 'aws-cdk-lib/aws-cloudfront-origins';
 import * as cloudfront from 'aws-cdk-lib/aws-cloudfront';
-import { RemovalPolicy } from 'aws-cdk-lib';
 import * as s3deploy from 'aws-cdk-lib/aws-s3-deployment';
 import { CacheControl } from 'aws-cdk-lib/aws-codepipeline-actions';
 import * as iam from 'aws-cdk-lib/aws-iam';
+import * as crypto from 'crypto';
 
 export interface InfraStackProps extends cdk.StackProps {
   appName: string;
@@ -16,93 +17,101 @@ export class InfraStack extends cdk.Stack {
     super(scope, id, props);
 
     const { appName } = props;
+    const { account, region } = this;
+
+    const hash = crypto
+      .createHash('md5')
+      .update(new Date().getTime().toString())
+      .digest('hex');
+
+    const s3BucketName = appName;
+
+    const distoribution = new cloudfront.Distribution(this, 'CloudFront', {
+      comment: `for ${appName}`,
+      defaultBehavior: {
+        origin: new origin.HttpOrigin(
+          `${s3BucketName}.s3-website-${region}.amazonaws.com`,
+          {
+            customHeaders: {
+              Referer: hash,
+            },
+            protocolPolicy: cloudfront.OriginProtocolPolicy.HTTP_ONLY,
+          },
+        ),
+        cachePolicy: cloudfront.CachePolicy.CACHING_DISABLED,
+      },
+      enableIpv6: false,
+      defaultRootObject: 'index.html',
+    });
 
     const s3bucket = new s3.Bucket(this, 'S3Bucket', {
-      bucketName: appName,
+      bucketName: s3BucketName,
       versioned: false,
-      removalPolicy: RemovalPolicy.DESTROY,
-      autoDeleteObjects: false,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      autoDeleteObjects: true,
       accessControl: s3.BucketAccessControl.PRIVATE,
       publicReadAccess: false,
+      websiteIndexDocument: 'index.html',
     });
 
-    // CloudFront Functionリソースの定義
-    const websiteIndexPageForwardFunction = new cloudfront.Function(
-      this,
-      'WebsiteIndexPageForwardFunction',
-      {
-        functionName: `${appName}-index-page-forwarder`,
-        code: cloudfront.FunctionCode.fromFile({
-          filePath: 'function/index.js',
+    const deployRole = new iam.Role(this, 'DeployWebsiteRole', {
+      roleName: `${appName}-deploy-role`,
+      assumedBy: new iam.ServicePrincipal('lambda.amazonaws.com'),
+      inlinePolicies: {
+        's3-policy': new iam.PolicyDocument({
+          statements: [
+            new iam.PolicyStatement({
+              effect: iam.Effect.ALLOW,
+              actions: ['s3:*'],
+              resources: [`${s3bucket.bucketArn}/`, `${s3bucket.bucketArn}/*`],
+            }),
+          ],
         }),
       },
-    );
-
-    const oac = new cloudfront.CfnOriginAccessControl(
-      this,
-      'OriginAccessControl',
-      {
-        originAccessControlConfig: {
-          name: `${appName}-oac`,
-          originAccessControlOriginType: 's3',
-          signingBehavior: 'no-override',
-          signingProtocol: 'sigv4',
-        },
-      },
-    );
-
-    const cf = new cloudfront.CloudFrontWebDistribution(this, 'CloudFront', {
-      comment: `for ${appName}`,
-      originConfigs: [
-        {
-          s3OriginSource: {
-            s3BucketSource: s3bucket,
-          },
-          behaviors: [
-            {
-              isDefaultBehavior: true,
-              compress: true,
-              functionAssociations: [
-                {
-                  eventType: cloudfront.FunctionEventType.VIEWER_REQUEST,
-                  function: websiteIndexPageForwardFunction,
-                },
-              ],
-              cachedMethods: cloudfront.CloudFrontAllowedCachedMethods.GET_HEAD,
-              viewerProtocolPolicy:
-                cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
-              minTtl: cdk.Duration.seconds(0),
-              maxTtl: cdk.Duration.seconds(86400),
-              defaultTtl: cdk.Duration.seconds(3600),
-            },
-          ],
-        },
-      ],
-      httpVersion: cloudfront.HttpVersion.HTTP2_AND_3,
     });
+
     s3bucket.addToResourcePolicy(
       new iam.PolicyStatement({
-        sid: 'AllowCloudFrontServicePrincipalReadOnly',
-        effect: iam.Effect.ALLOW,
-        principals: [new iam.ServicePrincipal('cloudfront.amazonaws.com')],
-        actions: ['s3:GetObject'],
+        effect: iam.Effect.DENY,
+        actions: ['s3:*'],
+        principals: [new iam.StarPrincipal()],
         resources: [`${s3bucket.bucketArn}/*`],
         conditions: {
-          StringEquals: {
-            'AWS:SourceArn': `arn:aws:cloudfront::${this.account}:distribution/${cf.distributionId}`,
+          StringNotLike: {
+            'aws:Referer': hash,
+          },
+          StringNotEquals: {
+            's3:ResourceAccount': account,
+            'aws:PrincipalArn': new iam.ArnPrincipal(deployRole.roleArn).arn,
           },
         },
       }),
     );
-
-    const cfnDistribution = cf.node.defaultChild as cloudfront.CfnDistribution;
-    cfnDistribution.addPropertyOverride(
-      'DistributionConfig.Origins.0.OriginAccessControlId',
-      oac.getAtt('Id'),
+    s3bucket.addToResourcePolicy(
+      new iam.PolicyStatement({
+        effect: iam.Effect.ALLOW,
+        actions: ['s3:GetObject'],
+        principals: [new iam.StarPrincipal()],
+        resources: [`${s3bucket.bucketArn}/*`],
+        conditions: {
+          StringLike: {
+            'aws:Referer': hash,
+          },
+        },
+      }),
     );
-    cfnDistribution.addPropertyOverride(
-      'DistributionConfig.Origins.0.S3OriginConfig.OriginAccessIdentity',
-      '',
+    s3bucket.addToResourcePolicy(
+      new iam.PolicyStatement({
+        effect: iam.Effect.ALLOW,
+        actions: ['s3:*'],
+        principals: [new iam.AccountPrincipal(account)],
+        resources: [`${s3bucket.bucketArn}/*`],
+        conditions: {
+          StringEquals: {
+            's3:ResourceAccount': account,
+          },
+        },
+      }),
     );
 
     // eslint-disable-next-line no-new
