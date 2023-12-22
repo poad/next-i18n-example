2be8c61c5db53721a2dee762162ae1ddb1e25dcf
Kenji Saito <ken-yo@mbr.nifty.com>
2023-12-22 21:10:45 +0900

    Bumps node modules

--- infra/function/index.ts
+++ infra/function/index.ts
@@ -6,10 +6,9 @@ interface CloudFrontRequestEvent {
   request: CroudFrontRequest;
 }
 function handler(event: CloudFrontRequestEvent) {
+  const request = event.request;
   // eslint-disable-next-line no-var
-  var { request } = event;
-  // eslint-disable-next-line no-var
-  var { uri } = request;
+  const uri = request.uri;
 
   // Check whether the URI is missing a file name.
   if (uri.endsWith('/')) {
